ed763933a721da1aadd223257e564fd836221429
IvanArco <ivan.arcos.garcia@gmail.com>
2026-06-27 12:27:29 -0600

    Implementacion de traduccionen Node

--- node/01_soap_client/index.js
+++ node/01_soap_client/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import soap from 'soap';
+import { translate } from '@vitalets/google-translate-api';
 
 const app = express();
 const port = 8201;
@@ -12,12 +13,16 @@ app.get('/', async (req, res) => {
   try {
     const client = await soap.createClientAsync(wsdl);
     const [result] = await client.NumberToWordsAsync({ ubiNum: Number(n) });
-    res.send((result.NumberToWordsResult || '').trim());
+
+    const english = (result.NumberToWordsResult || '').trim();
+    const translated = await translate(english, { from: 'en', to: 'es' });
+
+    res.send(translated.text);
   } catch (err) {
-    res.status(500).send(`Error SOAP: ${err.message}`);
+    res.status(500).send(`Error traduccion: ${err.message}`);
   }
 });
 
-app.listen(port, () => {
+app.listen(port, () => {  
   console.log(`Servidor Node SOAP en http://localhost:${port}`);
 });
