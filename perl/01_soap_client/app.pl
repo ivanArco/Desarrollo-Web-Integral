7302198fc756a3f659166a62c14253c034606080
IvanArco <ivan.arcos.garcia@gmail.com>
2026-06-28 13:16:44 -0600

    Implementacion de cambio de numero a letra mediante codigo en Perl. Parte 3

--- perl/01_soap_client/app.pl
+++ perl/01_soap_client/app.pl
@@ -1,13 +1,8 @@
 use strict;
 use warnings;
 use Mojolicious::Lite -signatures;
-use Mojo::UserAgent;
-use SOAP::Lite;
 
-my $wsdl = 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL';
-my $ua   = Mojo::UserAgent->new;
-
-sub to_es_local {
+sub to_es {
     my ($n) = @_;
     return 'cero' if $n == 0;
 
@@ -26,49 +21,22 @@ sub to_es_local {
     if ($n < 1000) {
         my $cen = int($n / 100);
         my $rest = $n % 100;
-        return $rest ? $c[$cen] . ' ' . to_es_local($rest) : $c[$cen];
+        return $rest ? $c[$cen] . ' ' . to_es($rest) : $c[$cen];
     }
     if ($n < 1000000) {
         my $mil = int($n / 1000);
         my $rest = $n % 1000;
-        my $left = $mil == 1 ? 'mil' : to_es_local($mil) . ' mil';
-        return $rest ? $left . ' ' . to_es_local($rest) : $left;
+        my $left = $mil == 1 ? 'mil' : to_es($mil) . ' mil';
+        return $rest ? $left . ' ' . to_es($rest) : $left;
     }
 
-    return undef;
-}
-
-sub translate_en_to_es {
-    my ($text) = @_;
-    my $tx = $ua->post(
-        'https://libretranslate.de/translate' =>
-        { Accept => 'application/json' } =>
-        json => {
-            q      => $text,
-            source => 'en',
-            target => 'es',
-            format => 'text'
-        }
-    );
-
-    return undef unless $tx->result->is_success;
-    my $json = $tx->result->json;
-    return undef unless ref $json eq 'HASH';
-    return $json->{translatedText};
+    return 'fuera de rango';
 }
 
 get '/' => sub ($c) {
     my $n = $c->param('n');
     return $c->render(text => 'Falta parametro n', status => 400) unless defined $n && $n =~ /^\d+$/;
-
-    my $soap = SOAP::Lite->service($wsdl);
-    my $english = $soap->NumberToWords($n + 0) // '';
-    my $spanish = translate_en_to_es($english);
-
-    # Fallback local si la API de traduccion no responde.
-    $spanish //= to_es_local($n + 0);
-
-    $c->render(text => $spanish // 'Sin traduccion');
+    $c->render(text => to_es($n + 0));
 };
 
 app->start;
