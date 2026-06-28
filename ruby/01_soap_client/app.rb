4005be51a87f3516e1287ba617f8fd5c8b678422
IvanArco <ivan.arcos.garcia@gmail.com>
2026-06-28 14:20:30 -0600

    Implementacion de cliente para servicio SOAP en Ruby. Parte 1

---
+++ ruby/01_soap_client/app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'savon'
+
+set :port, 8101
+
+SOAP_HOST = 'www.dataaccess.com'
+SOAP_IP = '52.7.155.169'
+WSDL_URL = "https://#{SOAP_IP}/webservicesserver/NumberConversion.wso?WSDL"
+SOAP_ENDPOINT = "https://#{SOAP_IP}/webservicesserver/NumberConversion.wso"
+
+get '/' do
+  n = params['n']
+  halt 400, 'Falta parametro n' unless n
+  halt 400, 'Parametro n debe ser numerico' unless n.match?(/^\d+$/)
+
+  # Workaround for environments where IPv6 to dataaccess.com times out.
+  client = Savon.client(
+    wsdl: WSDL_URL,
+    endpoint: SOAP_ENDPOINT,
+    namespace: 'http://www.dataaccess.com/webservicesserver/',
+    headers: { 'Host' => SOAP_HOST },
+    ssl_verify_mode: :none,
+    open_timeout: 10,
+    read_timeout: 10,
+    log: false
+  )
+  response = client.call(:number_to_words, message: { 'ubiNum' => n.to_i })
+  result = response.body.dig(:number_to_words_response, :number_to_words_result)
+
+  result.to_s.strip
+rescue StandardError => e
+  status 500
+  "Error SOAP: #{e.message}"
+end
