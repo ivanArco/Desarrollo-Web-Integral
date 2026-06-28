3c038d9b65036113e10dbeaa051a65f39e87c7bb
IvanArco <ivan.arcos.garcia@gmail.com>
2026-06-28 15:08:52 -0600

    Implementacion de traduccion de numeros a letras mediante codigo en Ruby. Parte 3

--- ruby/01_soap_client/app.rb
+++ ruby/01_soap_client/app.rb
@@ -1,43 +1,39 @@
 require 'sinatra'
-require 'savon'
-require 'json'
-require 'net/http'
-require 'uri'
 
-set :port, 8101
+set :port, 8103
 
-SOAP_HOST = 'www.dataaccess.com'
-SOAP_IP = '52.7.155.169'
-WSDL_URL = "https://#{SOAP_IP}/webservicesserver/NumberConversion.wso?WSDL"
-SOAP_ENDPOINT = "https://#{SOAP_IP}/webservicesserver/NumberConversion.wso"
+def to_es(n)
+  return 'cero' if n == 0
 
-class GoogleTranslate
-  def initialize(from:, to:)
-    @from = from
-    @to = to
+  units = %w[cero uno dos tres cuatro cinco seis siete ocho nueve diez once doce trece catorce quince dieciseis diecisiete dieciocho diecinueve]
+  tens = ['', '', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa']
+  hundreds = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos']
+
+  return units[n] if n < 20
+  return n == 20 ? 'veinte' : "veinti#{units[n - 20]}" if n < 30
+
+  if n < 100
+    dec = n / 10
+    uni = n % 10
+    return uni.zero? ? tens[dec] : "#{tens[dec]} y #{units[uni]}"
   end
 
-  def translate(text)
-    return '' if text.to_s.strip.empty?
+  return 'cien' if n == 100
 
-    params = {
-      client: 'gtx',
-      sl: @from,
-      tl: @to,
-      dt: 't',
-      q: text
-    }
-
-    uri = URI('https://translate.googleapis.com/translate_a/single')
-    uri.query = URI.encode_www_form(params)
-
-    response = Net::HTTP.get_response(uri)
-    raise "Error de traduccion HTTP #{response.code}" unless response.is_a?(Net::HTTPSuccess)
-
-    payload = JSON.parse(response.body)
-    chunks = payload[0] || []
-    chunks.map { |item| item[0] }.join
+  if n < 1000
+    cen = n / 100
+    rest = n % 100
+    return rest.zero? ? hundreds[cen] : "#{hundreds[cen]} #{to_es(rest)}"
   end
+
+  if n < 1_000_000
+    mil = n / 1000
+    rest = n % 1000
+    left = mil == 1 ? 'mil' : "#{to_es(mil)} mil"
+    return rest.zero? ? left : "#{left} #{to_es(rest)}"
+  end
+
+  'fuera de rango'
 end
 
 get '/' do
@@ -45,25 +41,8 @@ get '/' do
   halt 400, 'Falta parametro n' unless n
   halt 400, 'Parametro n debe ser numerico' unless n.match?(/^\d+$/)
 
-  # Workaround for environments where IPv6 to dataaccess.com times out.   
-  client = Savon.client(
-    wsdl: WSDL_URL,
-    endpoint: SOAP_ENDPOINT,
-    namespace: 'http://www.dataaccess.com/webservicesserver/',
-    headers: { 'Host' => SOAP_HOST },
-    ssl_verify_mode: :none,
-    open_timeout: 10,
-    read_timeout: 10,
-    log: false
-  )
-  response = client.call(:number_to_words, message: { 'ubiNum' => n.to_i })
-  english_text = response.body.dig(:number_to_words_response, :number_to_words_result).to_s.strip
-
-  translator = GoogleTranslate.new(from: 'en', to: 'es')
-  spanish_text = translator.translate(english_text)
-
-  spanish_text.to_s.strip
+  to_es(n.to_i)
 rescue StandardError => e
   status 500
-  "Error SOAP: #{e.message}"
+  "Error conversion local: #{e.message}"
 end
